b296ca6c993d24d870f6a4f83ca8cc1a51829096
abbastati27 <tati.abbu27@gmail.com>
2025-12-06 19:24:44 +0530

    GitHub Pages routing

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: "/ai-engineer-portfolio/",
+  base: "/portfolio/",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
